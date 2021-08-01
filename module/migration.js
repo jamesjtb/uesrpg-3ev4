4a1880de90a528f47b97f0af9c84be44d1926d73
Alex Hernandez <alexrhern@gmail.com>
2021-08-01 15:49:18 +0000

    Update module/migration.js

---
+++ module/migration.js
@@ -0,0 +1,204 @@
+/**
+ * Perform a system migration for the entire World, applying migrations for Actors, Items, and Compendium packs
+ * @return {Promise}      A Promise which resolves once the migration is completed
+ */
+export const migrateWorld = async function() {
+  ui.notifications.info(`Applying UESRPG System Migration for version ${game.system.data.version}. Please be patient and do not close your game or shut down your server.`, {permanent: true});
+
+  // Migrate World Actors
+  for ( let a of game.actors.contents ) {
+    try {
+      const updateData = migrateActorData(a.data);
+      if ( !foundry.utils.isObjectEmpty(updateData) ) {
+        console.log(`Migrating Actor entity ${a.name}`);
+        await a.update(updateData, {enforceTypes: false});
+      }
+    } catch(err) {
+      err.message = `Failed UESRPG system migration for Actor ${a.name}: ${err.message}`;
+      console.error(err);
+    }
+  }
+
+  // Migrate World Items
+  for ( let i of game.items.contents ) {
+    try {
+      const updateData = migrateItemData(i.toObject());
+      if ( !foundry.utils.isObjectEmpty(updateData) ) {
+        console.log(`Migrating Item entity ${i.name}`);
+        await i.update(updateData, {enforceTypes: false});
+      }
+    } catch(err) {
+      err.message = `Failed UESRPG system migration for Item ${i.name}: ${err.message}`;
+      console.error(err);
+    }
+  }
+
+  // Migrate Actor Override Tokens
+  for ( let s of game.scenes.contents ) {
+    try {
+      const updateData = migrateSceneData(s.data);
+      if ( !foundry.utils.isObjectEmpty(updateData) ) {
+        console.log(`Migrating Scene entity ${s.name}`);
+        await s.update(updateData, {enforceTypes: false});
+        // If we do not do this, then synthetic token actors remain in cache
+        // with the un-updated actorData.
+        s.tokens.contents.forEach(t => t._actor = null);
+      }
+    } catch(err) {
+      err.message = `Failed UESRPG system migration for Scene ${s.name}: ${err.message}`;
+      console.error(err);
+    }
+  }
+
+  // Migrate World Compendium Packs
+  for ( let p of game.packs ) {
+    if ( p.metadata.package !== "world" ) continue;
+    if ( !["Actor", "Item", "Scene"].includes(p.metadata.entity) ) continue;
+    await migrateCompendium(p);
+  }
+
+  // Set the migration as complete
+	game.settings.set("uesrpg-d100", "systemMigrationVersion", game.system.data.version);
+  ui.notifications.info(`UESRPG System Migration to version ${game.system.data.version} completed!`, {permanent: true});
+};
+
+/* -------------------------------------------- */
+
+/**
+ * Apply migration rules to all Entities within a single Compendium pack
+ * @param pack
+ * @return {Promise}
+ */
+export const migrateCompendium = async function(pack) {
+  const entity = pack.metadata.entity;
+  if ( !["Actor", "Item", "Scene"].includes(entity) ) return;
+
+  // Unlock the pack for editing
+  const wasLocked = pack.locked;
+  await pack.configure({locked: false});
+
+  // Begin by requesting server-side data model migration and get the migrated content
+  await pack.migrate();
+  const documents = await pack.getDocuments();
+
+  // Iterate over compendium entries - applying fine-tuned migration functions
+  for ( let doc of documents ) {
+    let updateData = {};
+    try {
+      switch (entity) {
+        case "Actor":
+          updateData = migrateActorData(doc.data);
+          break;
+        case "Item":
+          updateData = migrateItemData(doc.toObject());
+          break;
+        case "Scene":
+          updateData = migrateSceneData(doc.data);
+          break;
+      }
+
+      // Save the entry, if data was changed
+      if ( foundry.utils.isObjectEmpty(updateData) ) continue;
+      await doc.update(updateData);
+      console.log(`Migrated ${entity} entity ${doc.name} in Compendium ${pack.collection}`);
+    }
+
+    // Handle migration failures
+    catch(err) {
+      err.message = `Failed UESRPG system migration for entity ${doc.name} in pack ${pack.collection}: ${err.message}`;
+      console.error(err);
+    }
+  }
+
+  // Apply the original locked status for the pack
+  await pack.configure({locked: wasLocked});
+  console.log(`Migrated all ${entity} entities from Compendium ${pack.collection}`);
+};
+
+/* -------------------------------------------- */
+/*  Entity Type Migration Helpers               */
+/* -------------------------------------------- */
+
+/**
+ * Migrate a single Actor entity to incorporate latest data model changes
+ * Return an Object of updateData to be applied
+ * @param {object} actor    The actor data object to update
+ * @return {Object}         The updateData to apply
+ */
+export const migrateActorData = function(actor) {
+  const updateData = {};
+
+  // Actor Data Updates
+  if (actor.data) {
+  }
+
+  // Migrate Owned Items
+  if ( !actor.items ) return updateData;
+  const items = actor.items.reduce((arr, i) => {
+    // Migrate the Owned Item
+    const itemData = i instanceof CONFIG.Item.documentClass ? i.toObject() : i;
+    let itemUpdate = migrateItemData(itemData);
+
+    // Update the Owned Item
+    if ( !isObjectEmpty(itemUpdate) ) {
+      itemUpdate._id = itemData._id;
+      arr.push(expandObject(itemUpdate));
+    }
+
+    return arr;
+  }, []);
+  if ( items.length > 0 ) updateData.items = items;
+  return updateData;
+};
+
+/**
+ * Migrate a single Item entity to incorporate latest data model changes
+ *
+ * @param {object} item  Item data to migrate
+ * @return {object}      The updateData to apply
+ */
+export const migrateItemData = function(item) {
+  const updateData = {};
+	
+	if (item.img.startsWith("systems/uesrpg-d100/images/Icons/") && (item.img.endsWith(".png") || item.img.endsWiths(".jpg"))) {
+		updateData["img"] = item.img.replace(".png", ".webp").replace(".jpg", ".webp");
+	}
+  return updateData;
+};
+
+/**
+ * Migrate a single Scene entity to incorporate changes to the data model of it's actor data overrides
+ * Return an Object of updateData to be applied
+ * @param {Object} scene  The Scene data to Update
+ * @return {Object}       The updateData to apply
+ */
+export const migrateSceneData = function(scene) {
+  const tokens = scene.tokens.map(token => {
+    const t = token.toJSON();
+    if (!t.actorId || t.actorLink) {
+      t.actorData = {};
+    }
+    else if ( !game.actors.has(t.actorId) ){
+      t.actorId = null;
+      t.actorData = {};
+    }
+    else if ( !t.actorLink ) {
+      const actorData = duplicate(t.actorData);
+      actorData.type = token.actor?.type;
+      const update = migrateActorData(actorData);
+      ['items', 'effects'].forEach(embeddedName => {
+        if (!update[embeddedName]?.length) return;
+        const updates = new Map(update[embeddedName].map(u => [u._id, u]));
+        t.actorData[embeddedName].forEach(original => {
+          const update = updates.get(original._id);
+          if (update) mergeObject(original, update);
+        });
+        delete update[embeddedName];
+      });
+
+      mergeObject(t.actorData, update);
+    }
+    return t;
+  });
+  return {tokens};
+};
