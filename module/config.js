c75f4453baba71274d9829e65f7d944d7d888c3e
Alex Hernandez <alexrhern@gmail.com>
2021-08-01 15:49:47 +0000

    Update module/config.js

---
+++ module/config.js
@@ -0,0 +1,17 @@
+export const UESRPG = {};
+
+UESRPG.languages = {
+	"aldmeri": "Aldmeri",
+	"ayleidoon": "Ayleidoon",
+	"bosmeri": "Bosmeri",
+	"cyrodilic": "Cyrodilic",
+	"daedric": "Daedric",
+	"dovah": "Dovah",
+	"dunmeri": "Dunmeri",
+	"dwemeris": "Dwemeris",
+	"falmer": "Falmer",
+	"jel": "Jel",
+	"nordic": "Nordic",
+	"taagra": "Ta'Agra",
+	"yoku": "Yoku"
+}
