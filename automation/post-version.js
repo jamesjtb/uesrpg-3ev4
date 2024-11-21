174bf5805b71138db8493def8d21a98562e8591b
James J <jamesj.tb@gmail.com>
2024-11-20 23:32:15 -0500

    added post-versioning script

---
+++ automation/post-version.js
@@ -0,0 +1,19 @@
+const {writeFileSync, readFileSync} = require('fs');
+const packageObj = require('../package.json');
+
+const systemFilePath = './system.json';
+const systemFileEncoding = 'utf-8';
+const packageVersion = packageObj.version;
+
+console.log(`Updating system.json with version '${packageVersion}'`);
+const systemJson = readFileSync(systemFilePath, systemFileEncoding);
+const systemObj = JSON.parse(systemJson);
+
+const splitDownloadPath = systemObj.download.split('/');
+const lastElement = splitDownloadPath.length - 1;
+splitDownloadPath[lastElement] = `v${packageVersion}.zip`;
+
+systemObj.version = packageVersion;
+systemObj.download = splitDownloadPath.join('/');
+
+writeFileSync(systemFilePath, JSON.stringify(systemObj, null, 2), systemFileEncoding);
