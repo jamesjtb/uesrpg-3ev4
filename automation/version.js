0399c75d746252e1d0e99b95fa2cf9f128ffe946
James J <jamesj.tb@gmail.com>
2024-11-21 03:18:30 -0500

    fix download and manifest url

--- automation/version.js
+++ automation/version.js
@@ -12,11 +12,14 @@ console.log(`Updating system.json with version '${packageVersion}'`);
 const systemJson = readFileSync(systemFilePath, systemFileEncoding);
 const systemObj = JSON.parse(systemJson);
 
+const manifestUrl = `https://raw.githubusercontent.com/jamesjtb/uesrpg-3ev4/refs/tags/${packageVersion}/system.json`
+
 const splitDownloadPath = systemObj.download.split('/');
 const lastElement = splitDownloadPath.length - 1;
-splitDownloadPath[lastElement] = `v${packageVersion}.zip`;
+splitDownloadPath[lastElement] = `${packageVersion}.zip`;
 
 systemObj.version = packageVersion;
+systemObj.manifest = manifestUrl;
 systemObj.download = splitDownloadPath.join('/');
 
 writeFileSync(systemFilePath, JSON.stringify(systemObj, null, 2), systemFileEncoding);
