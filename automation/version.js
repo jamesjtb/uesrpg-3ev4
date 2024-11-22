d2fe70f3266ceb0a6eff1a159cd62f8fdbed1300
James Johnes <jamesdabone@gmail.com>
2024-11-22 13:17:30 -0500

    use new downlaod path

--- automation/version.js
+++ automation/version.js
@@ -12,15 +12,14 @@ console.log(`Updating system.json with version '${packageVersion}'`);
 const systemJson = readFileSync(systemFilePath, systemFileEncoding);
 const systemObj = JSON.parse(systemJson);
 
-const manifestUrl = `https://raw.githubusercontent.com/jamesjtb/uesrpg-3ev4/refs/tags/${packageVersion}/system.json`
+const manifestUrl = `https://raw.githubusercontent.com/jamesjtb/uesrpg-3ev4/refs/tags/${packageVersion}/system.json`;
 
-const splitDownloadPath = systemObj.download.split('/');
-const lastElement = splitDownloadPath.length - 1;
-splitDownloadPath[lastElement] = `${packageVersion}.zip`;
+const downloadPath = `https://github.com/jamesjtb/uesrpg-3ev4/releases/download/${packageVersion}/uesrpg-bundle_${packageVersion}.zip`;
+
 
 systemObj.version = packageVersion;
 systemObj.manifest = manifestUrl;
-systemObj.download = splitDownloadPath.join('/');
+systemObj.download = downloadPath;
 
 writeFileSync(systemFilePath, JSON.stringify(systemObj, null, 2), systemFileEncoding);
 
