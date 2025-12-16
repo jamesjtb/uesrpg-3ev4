1b6053f18c33a2667796088591ef5b8afe8a542d
varys1337 <varys1337@gmail.com>
2025-12-16 16:47:16 +0300

    Execute migrations on application ready
    Added migration execution on ready hook.

--- module/entrypoint.js
+++ module/entrypoint.js
@@ -3,9 +3,11 @@ import initHandler from './handlers/init.js';
 import { runMigrations } from './migrations.js';
 Hooks.once('ready', async function () {
   console.log(`UESRPG | Ready`);
+Hooks.once('ready', async function () {
+  console.log(`UESRPG | Ready`);
+  await runMigrations();
   await startupHandler();
 });
-
 Hooks.once("init", async function() {
   console.log(`UESRPG | Initializing`);
   await initHandler();
