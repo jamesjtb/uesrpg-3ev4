912b51cc985527f4c6364f1a1ea2cae08fa37705
varys1337 <varys1337@gmail.com>
2025-12-16 18:00:12 +0300

    Update constants.js

--- module/constants.js
+++ module/constants.js
@@ -1,19 +1,36 @@
 export const UESRPG = {};
 
+/**
+ * Languages
+ */
 UESRPG.languages = {
-	"aldmeri": "Aldmeri",
-	"ayleidoon": "Ayleidoon",
-	"bosmeri": "Bosmeri",
-	"cyrodilic": "Cyrodilic",
-	"daedric": "Daedric",
-	"dovah": "Dovah",
-	"dunmeri": "Dunmeri",
-	"dwemeris": "Dwemeris",
-	"falmer": "Falmer",
-	"jel": "Jel",
-	"nordic": "Nordic",
-	"taagra": "Ta'Agra",
-	"yoku": "Yoku"
-}
+  "aldmeri": "Aldmeri",
+  "ayleidoon": "Ayleidoon",
+  "bosmeri": "Bosmeri",
+  "cyrodilic": "Cyrodilic",
+  "daedric": "Daedric",
+  "dovah": "Dovah",
+  "dunmeri": "Dunmeri",
+  "dwemeris": "Dwemeris",
+  "falmer": "Falmer",
+  "jel": "Jel",
+  "nordic": "Nordic",
+  "taagra": "Ta'Agra",
+  "yoku": "Yoku"
+};
 
-export const systemRootPath = 'systems/uesrpg-3ev4/';
+/**
+ * Armor hit-location categories for Armor Items.
+ * These keys MUST match actor.js bucketing logic and template.json expectations.
+ */
+UESRPG.armorItemCat = {
+  head: "Head",
+  body: "Body",
+  r_arm: "Right Arm",
+  l_arm: "Left Arm",
+  r_leg: "Right Leg",
+  l_leg: "Left Leg",
+  shield: "Shield"
+};
+
+export const systemRootPath = "systems/uesrpg-3ev4/";
