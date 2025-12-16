99fe8e8876d64c0d0bfcd80e1dfdd547e464b342
varys1337 <varys1337@gmail.com>
2025-12-16 16:56:50 +0300

    Update entrypoint.js

--- module/entrypoint.js
+++ module/entrypoint.js
@@ -1,14 +1,14 @@
 import startupHandler from './handlers/startup.js';
 import initHandler from './handlers/init.js';
 import { runMigrations } from './migrations.js';
-Hooks.once('ready', async function () {
-  console.log(`UESRPG | Ready`);
+
+Hooks.once("init", async function() {
+  console.log(`UESRPG | Initializing`);
+  await initHandler();
+});
+
 Hooks.once('ready', async function () {
   console.log(`UESRPG | Ready`);
   await runMigrations();
   await startupHandler();
-});
-Hooks.once("init", async function() {
-  console.log(`UESRPG | Initializing`);
-  await initHandler();
 });
