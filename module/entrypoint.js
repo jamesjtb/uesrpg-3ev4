7233d6694b5c8f4fab5c393633c7c264d08fe5e5
varys1337 <varys1337@gmail.com>
2025-12-17 12:53:43 +0300

    Format entrypoint.js for consistency

--- module/entrypoint.js
+++ module/entrypoint.js
@@ -1,16 +1,16 @@
-1: import startupHandler from './handlers/startup.js';
-2: import initHandler from './handlers/init.js';
-3: import { runMigrations } from './migrations.js';
-4: import { initAutomatedCombat } from './combat/automated-combat.js';
+ import startupHandler from './handlers/startup.js';
+ import initHandler from './handlers/init.js';
+ import { runMigrations } from './migrations.js';
+ import { initAutomatedCombat } from './combat/automated-combat.js';
 
-6: Hooks.once("init", async function() {
-7:   console.log(`UESRPG | Initializing`);
-8:   await initHandler();
-9: });
+ Hooks.once("init", async function() {
+   console.log(`UESRPG | Initializing`);
+   await initHandler();
+ });
 
-11: Hooks.once('ready', async function () {
-12:   console.log(`UESRPG | Ready`);
-13:   initAutomatedCombat();
-14:   await runMigrations();
-15:   await startupHandler();
-16: });
+ Hooks.once('ready', async function () {
+   console.log(`UESRPG | Ready`);
+   initAutomatedCombat();
+   await runMigrations();
+   await startupHandler();
+ });
