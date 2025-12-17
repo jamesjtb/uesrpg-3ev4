989dfc84d59a8fc182ab6dac6c4099af4261c54c
varys1337 <varys1337@gmail.com>
2025-12-17 12:36:41 +0300

    Add automated combat initialization to entrypoint

--- module/entrypoint.js
+++ module/entrypoint.js
@@ -1,14 +1,16 @@
-import startupHandler from './handlers/startup.js';
-import initHandler from './handlers/init.js';
-import { runMigrations } from './migrations.js';
+1: import startupHandler from './handlers/startup.js';
+2: import initHandler from './handlers/init.js';
+3: import { runMigrations } from './migrations.js';
+4: import { initAutomatedCombat } from './combat/automated-combat.js';
 
-Hooks.once("init", async function() {
-  console.log(`UESRPG | Initializing`);
-  await initHandler();
-});
+6: Hooks.once("init", async function() {
+7:   console.log(`UESRPG | Initializing`);
+8:   await initHandler();
+9: });
 
-Hooks.once('ready', async function () {
-  console.log(`UESRPG | Ready`);
-  await runMigrations();
-  await startupHandler();
-});
+11: Hooks.once('ready', async function () {
+12:   console.log(`UESRPG | Ready`);
+13:   initAutomatedCombat();
+14:   await runMigrations();
+15:   await startupHandler();
+16: });
