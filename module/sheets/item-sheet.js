983b3d143b8a2c626f6b7bbaa9903a1392fc50f6
varys1337 <varys1337@gmail.com>
2025-12-19 23:29:40 +0300

    Extend SimpleItemSheet to use foundry.appv1

--- module/sheets/item-sheet.js
+++ module/sheets/item-sheet.js
@@ -2,7 +2,7 @@
  * Extend the basic ItemSheet with some very simple modifications
  * @extends {ItemSheet}
  */
-export class SimpleItemSheet extends ItemSheet {
+export class SimpleItemSheet extends foundry.appv1.sheets.ItemSheet {
 
   /** @override */
 	static get defaultOptions() {
