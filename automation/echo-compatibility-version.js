1dceb1ea772822535c88bf3653c9506570f267d0
varys1337 <varys1337@gmail.com>
2026-01-12 15:04:20 +0300

    Delete automation directory

--- automation/echo-compatibility-version.js
+++
@@ -1,9 +0,0 @@
-const { argv } = require('node:process');
-const systemObj = require('../system.json');
-
-const requestedVersionType = argv[2];
-const validVersionTypes = ['minimum', 'verified', 'maximum']
-if (!validVersionTypes.includes(requestedVersionType)) {
-    throw new Error(`Invalid version type request: ${requestedVersionType}. Must be one of: ${validVersionTypes.join(', ')}`);
-}
-console.log(`${requestedVersionType}=${systemObj.compatibility[requestedVersionType]}`);
